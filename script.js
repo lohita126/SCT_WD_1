642199f1285d41ec1a14caf6be5fb6b4b35eba7d
lohita126 <lohitaclick@gmail.com>
2025-11-06 19:40:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Navbar color change on scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
